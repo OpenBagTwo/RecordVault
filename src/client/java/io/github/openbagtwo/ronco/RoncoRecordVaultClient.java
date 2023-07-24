e564dae6df3cd70870bef1cecf8449db5cb1ca4e
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2023-07-24 19:54:10 -0400

    Override "is it a book?" check to accept music discs

--- src/client/java/io/github/openbagtwo/ronco/RoncoRecordVaultClient.java
+++ src/client/java/io/github/openbagtwo/ronco/RoncoRecordVaultClient.java
@@ -2,7 +2,7 @@ package io.github.openbagtwo.ronco;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class RoncoRecordVaultClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
