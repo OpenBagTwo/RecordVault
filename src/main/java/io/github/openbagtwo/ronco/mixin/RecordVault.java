3d9e2ddd60a8ba5f350e5a64d0719813098720dc
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2023-07-24 20:59:53 -0400

    Override shelf interaction

--- src/main/java/io/github/openbagtwo/ronco/mixin/RecordVault.java
+++ src/main/java/io/github/openbagtwo/ronco/mixin/RecordVault.java
@@ -1,38 +1,91 @@
 package io.github.openbagtwo.ronco.mixin;
 
+import java.util.Optional;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ChiseledBookshelfBlock;
+import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.ChiseledBookshelfBlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.tag.ItemTags;
-import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.state.property.Property;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ChiseledBookshelfBlockEntity.class)
+@Mixin(ChiseledBookshelfBlock.class)
 public abstract class RecordVault {
 
-	@Shadow
-	DefaultedList<ItemStack> inventory;
+  @Invoker("getHitPos")
+  static Optional<Vec2f> getHitPos(BlockHitResult hit, Direction facing){
+    throw new AssertionError();
+  }
 
-	@Shadow
-	abstract void updateState(int slot);
+  @Invoker("getSlotForHitPos")
+  static int getSlotForHitPos(Vec2f hitPos){
+    throw new AssertionError();
+  }
 
-	@Overwrite
-	public boolean isValid(int slot, ItemStack stack) {
-		return (
-				stack.isIn(ItemTags.BOOKSHELF_BOOKS)
-				|| stack.isIn(ItemTags.MUSIC_DISCS)
-		) && ((ChiseledBookshelfBlockEntity)(Object)this).getStack(slot).isEmpty();
-	}
-	@Overwrite
-	public void setStack(int slot, ItemStack stack) {
-		if (
-				stack.isIn(ItemTags.BOOKSHELF_BOOKS)
-				|| stack.isIn(ItemTags.MUSIC_DISCS)
-		) {
-			this.inventory.set(slot, stack);
-			this.updateState(slot);
-		}
+  @Inject(
+      method="onUse(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;Lnet/minecraft/util/hit/BlockHitResult;)Lnet/minecraft/util/ActionResult;",
+      at=@At("HEAD"),
+      cancellable = true
+  )
+  public void onUsingRecord(
+      BlockState state,
+      World world,
+      BlockPos pos,
+      PlayerEntity player,
+      Hand hand,
+      BlockHitResult hit,
+      CallbackInfoReturnable<ActionResult> callbackInfo
+  ) {
+    BlockEntity maybeBookshelf = world.getBlockEntity(pos);
+    if (maybeBookshelf instanceof ChiseledBookshelfBlockEntity chiseledBookshelfBlockEntity) {
+      Optional shelf = this.getHitPos(hit, state.get(HorizontalFacingBlock.FACING));
+      if (!shelf.isEmpty()) {
+        int slot = this.getSlotForHitPos((Vec2f)shelf.get());
+        if (!(Boolean)state.get((Property)ChiseledBookshelfBlock.SLOT_OCCUPIED_PROPERTIES.get(slot)))
+        {
+          ItemStack itemStack = player.getStackInHand(hand);
+          if (itemStack.isIn(ItemTags.MUSIC_DISCS)) {
+            tryAddRecord(world, pos, player, chiseledBookshelfBlockEntity, itemStack, slot);
+            callbackInfo.setReturnValue(ActionResult.success(world.isClient));
+            callbackInfo.cancel();
+          }
+        }
+      }
+    }
+  }
 
-	}
+  private static void tryAddRecord(World world, BlockPos pos, PlayerEntity player, ChiseledBookshelfBlockEntity blockEntity, ItemStack stack, int slot) {
+    if (!world.isClient) {
+      // treat it like an enchanted book
+      player.incrementStat(Stats.USED.getOrCreateStat(stack.getItem()));
+      SoundEvent soundEvent = SoundEvents.BLOCK_CHISELED_BOOKSHELF_INSERT_ENCHANTED;
+      blockEntity.setStack(slot, stack.split(1));
+      world.playSound((PlayerEntity)null, pos, soundEvent, SoundCategory.BLOCKS, 1.0F, 1.0F);
+      if (player.isCreative()) {
+        stack.increment(1);
+      }
+      world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
+    }
+  }
+
 }
---
+++ src/main/java/io/github/openbagtwo/ronco/mixin/RecordVaultEntity.java
@@ -0,0 +1,38 @@
+package io.github.openbagtwo.ronco.mixin;
+
+import net.minecraft.block.entity.ChiseledBookshelfBlockEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.util.collection.DefaultedList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(ChiseledBookshelfBlockEntity.class)
+public abstract class RecordVaultEntity {
+
+	@Shadow
+	DefaultedList<ItemStack> inventory;
+
+	@Shadow
+	abstract void updateState(int slot);
+
+	@Overwrite
+	public boolean isValid(int slot, ItemStack stack) {
+		return (
+				stack.isIn(ItemTags.BOOKSHELF_BOOKS)
+				|| stack.isIn(ItemTags.MUSIC_DISCS)
+		) && ((ChiseledBookshelfBlockEntity)(Object)this).getStack(slot).isEmpty();
+	}
+	@Overwrite
+	public void setStack(int slot, ItemStack stack) {
+		if (
+				stack.isIn(ItemTags.BOOKSHELF_BOOKS)
+				|| stack.isIn(ItemTags.MUSIC_DISCS)
+		) {
+			this.inventory.set(slot, stack);
+			this.updateState(slot);
+		}
+
+	}
+}
