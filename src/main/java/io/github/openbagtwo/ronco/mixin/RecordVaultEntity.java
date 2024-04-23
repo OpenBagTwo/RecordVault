b6834b712ed2d0677227ac2ce54ab547a32dd01f
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2024-04-23 13:10:46 -0400

    Accept ALL music discs (don't check by tag)
    See: https://github.com/OpenBagTwo/FoxNap/issues/51

--- src/main/java/io/github/openbagtwo/ronco/mixin/RecordVaultEntity.java
+++ src/main/java/io/github/openbagtwo/ronco/mixin/RecordVaultEntity.java
@@ -2,11 +2,13 @@ package io.github.openbagtwo.ronco.mixin;
 
 import net.minecraft.block.entity.ChiseledBookshelfBlockEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.MusicDiscItem;
 import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(ChiseledBookshelfBlockEntity.class)
 public abstract class RecordVaultEntity {
@@ -17,19 +19,29 @@ public abstract class RecordVaultEntity {
 	@Shadow
 	abstract void updateState(int slot);
 
+	@Unique
+	private boolean isValidForBookshelf(ItemStack stack) {
+		return stack.isIn(ItemTags.BOOKSHELF_BOOKS) || (stack.getItem() instanceof MusicDiscItem);
+	}
+
+	/**
+	 * @author OpenBagTwo
+	 * @reason Allow any music disc (regardless of item tags) to go into the bookshelf
+	 */
 	@Overwrite
 	public boolean isValid(int slot, ItemStack stack) {
 		return (
-				stack.isIn(ItemTags.BOOKSHELF_BOOKS)
-				|| stack.isIn(ItemTags.MUSIC_DISCS)
-		) && ((ChiseledBookshelfBlockEntity)(Object)this).getStack(slot).isEmpty();
+				isValidForBookshelf(stack)
+						&& ((ChiseledBookshelfBlockEntity)(Object)this).getStack(slot).isEmpty()
+		);
 	}
+	/**
+	 * @author OpenBagTwo
+	 * @reason Allow any music disc (regardless of item tags) to go into the bookshelf
+	 */
 	@Overwrite
 	public void setStack(int slot, ItemStack stack) {
-		if (
-				stack.isIn(ItemTags.BOOKSHELF_BOOKS)
-				|| stack.isIn(ItemTags.MUSIC_DISCS)
-		) {
+		if (isValidForBookshelf(stack)) {
 			this.inventory.set(slot, stack);
 			this.updateState(slot);
 		}
