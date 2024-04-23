20b943982f9e03324a3a18e32f2ff3db147fddd7
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2024-04-23 13:51:25 -0400

    Cleanups and fixes

--- src/main/java/io/github/openbagtwo/ronco/mixin/RecordVault.java
+++ src/main/java/io/github/openbagtwo/ronco/mixin/RecordVault.java
@@ -1,6 +1,5 @@
 package io.github.openbagtwo.ronco.mixin;
 
-import java.util.Optional;
 import java.util.OptionalInt;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ChiseledBookshelfBlock;
@@ -14,12 +13,10 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
 import net.minecraft.state.property.Property;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.ItemActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec2f;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
@@ -30,15 +27,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ChiseledBookshelfBlock.class)
 public abstract class RecordVault {
 
-  @Invoker("getHitPos")
-  static Optional<Vec2f> getHitPos(BlockHitResult hit, Direction facing){
-    throw new AssertionError();
-  }
-
   @Invoker("getSlotForHitPos")
-  static OptionalInt getSlotForHitPos(BlockHitResult hit, BlockState state){
-    throw new AssertionError();
-  }
+  abstract OptionalInt getSlotForHitPos(BlockHitResult hit, BlockState state);
 
   @Inject(
       method="onUseWithItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;Lnet/minecraft/util/hit/BlockHitResult;)Lnet/minecraft/util/ItemActionResult;",
@@ -53,16 +43,16 @@ public abstract class RecordVault {
       PlayerEntity player,
       Hand hand,
       BlockHitResult hit,
-      CallbackInfoReturnable<ActionResult> callbackInfo
+      CallbackInfoReturnable<ItemActionResult> callbackInfo
   ) {
     BlockEntity maybeBookshelf = world.getBlockEntity(pos);
     if (maybeBookshelf instanceof ChiseledBookshelfBlockEntity chiseledBookshelfBlockEntity) {
       if (stack.getItem() instanceof MusicDiscItem){
         OptionalInt slot = this.getSlotForHitPos(hit, state);
-        if (slot.isEmpty()) {
-          if (!((Boolean)state.get((Property)ChiseledBookshelfBlock.SLOT_OCCUPIED_PROPERTIES.get(slot.getAsInt())))) {
+        if (!slot.isEmpty()) {
+          if (!((Boolean) state.get((Property)ChiseledBookshelfBlock.SLOT_OCCUPIED_PROPERTIES.get(slot.getAsInt())))) {
             tryAddRecord(world, pos, player, chiseledBookshelfBlockEntity, stack, slot.getAsInt());
-            callbackInfo.setReturnValue(ActionResult.success(world.isClient));
+            callbackInfo.setReturnValue(ItemActionResult.success(world.isClient));
             callbackInfo.cancel();
           }
         }
