dc4631ddfb68bcb014dcfbd14e8989706d0b7133
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2024-04-23 14:04:48 -0400

    Apply changes back to 1.20.1 port

--- src/main/java/io/github/openbagtwo/ronco/mixin/RecordVault.java
+++ src/main/java/io/github/openbagtwo/ronco/mixin/RecordVault.java
@@ -1,8 +1,9 @@
 package io.github.openbagtwo.ronco.mixin;
 
-import java.util.OptionalInt;
+import java.util.Optional;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ChiseledBookshelfBlock;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.ChiseledBookshelfBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -13,11 +14,14 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
 import net.minecraft.state.property.Property;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.ItemActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec2f;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
@@ -27,32 +31,41 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ChiseledBookshelfBlock.class)
 public abstract class RecordVault {
 
+  @Invoker("getHitPos")
+  static Optional<Vec2f> getHitPos(BlockHitResult hit, Direction facing){
+    throw new AssertionError();
+  }
+
   @Invoker("getSlotForHitPos")
-  abstract OptionalInt getSlotForHitPos(BlockHitResult hit, BlockState state);
+  static int getSlotForHitPos(Vec2f hitPos){
+    throw new AssertionError();
+  }
 
   @Inject(
-      method="onUseWithItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;Lnet/minecraft/util/hit/BlockHitResult;)Lnet/minecraft/util/ItemActionResult;",
+      method="onUse(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;Lnet/minecraft/util/hit/BlockHitResult;)Lnet/minecraft/util/ActionResult;",
       at=@At("HEAD"),
       cancellable = true
   )
   public void onUsingRecord(
-      ItemStack stack,
       BlockState state,
       World world,
       BlockPos pos,
       PlayerEntity player,
       Hand hand,
       BlockHitResult hit,
-      CallbackInfoReturnable<ItemActionResult> callbackInfo
+      CallbackInfoReturnable<ActionResult> callbackInfo
   ) {
     BlockEntity maybeBookshelf = world.getBlockEntity(pos);
     if (maybeBookshelf instanceof ChiseledBookshelfBlockEntity chiseledBookshelfBlockEntity) {
-      if (stack.getItem() instanceof MusicDiscItem){
-        OptionalInt slot = this.getSlotForHitPos(hit, state);
-        if (!slot.isEmpty()) {
-          if (!((Boolean) state.get((Property)ChiseledBookshelfBlock.SLOT_OCCUPIED_PROPERTIES.get(slot.getAsInt())))) {
-            tryAddRecord(world, pos, player, chiseledBookshelfBlockEntity, stack, slot.getAsInt());
-            callbackInfo.setReturnValue(ItemActionResult.success(world.isClient));
+      Optional shelf = this.getHitPos(hit, state.get(HorizontalFacingBlock.FACING));
+      if (!shelf.isEmpty()) {
+        int slot = this.getSlotForHitPos((Vec2f)shelf.get());
+        if (!(Boolean)state.get((Property)ChiseledBookshelfBlock.SLOT_OCCUPIED_PROPERTIES.get(slot)))
+        {
+          ItemStack itemStack = player.getStackInHand(hand);
+          if (itemStack.getItem() instanceof MusicDiscItem) {
+            tryAddRecord(world, pos, player, chiseledBookshelfBlockEntity, itemStack, slot);
+            callbackInfo.setReturnValue(ActionResult.success(world.isClient));
             callbackInfo.cancel();
           }
         }
@@ -70,6 +83,7 @@ public abstract class RecordVault {
       if (player.isCreative()) {
         stack.increment(1);
       }
+      world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
     }
   }
 
